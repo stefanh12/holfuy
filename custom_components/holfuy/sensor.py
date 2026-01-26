d02f35ce6de2fd0ab8b7c9ae50eca53d8e8711bc
Stefan <wan73605@burktom.anonaddy.com>
2026-01-26 09:40:30 +0100

    updated to device

--- custom_components/holfuy/sensor.py
+++ custom_components/holfuy/sensor.py
@@ -1,6 +1,6 @@
 from homeassistant.components.sensor import SensorEntity
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
-from .const import DOMAIN, CONF_WIND_UNIT, CONF_TEMP_UNIT, DEFAULT_WIND_UNIT, DEFAULT_TEMP_UNIT
+from .const import DOMAIN, CONF_WIND_UNIT, CONF_TEMP_UNIT, DEFAULT_WIND_UNIT, DEFAULT_TEMP_UNIT, CONF_STATION_ID
 
 # Keep sensor type names and device class where applicable; units will be set per-entry
 SENSOR_TYPES = {
@@ -26,6 +26,8 @@ async def async_setup_entry(hass, entry, async_add_entities):
     # For wind sensors use su directly (API and display strings use same values like "m/s", "km/h", "knots", "mph")
     wind_unit_display = su
 
+    station_id = entry.data.get(CONF_STATION_ID)
+
     for key, (name, device_class) in SENSOR_TYPES.items():
         if key == "temperature":
             unit = temp_display_unit
@@ -34,18 +36,25 @@ async def async_setup_entry(hass, entry, async_add_entities):
         else:
             unit = None
 
-        sensors.append(HolfuySensor(coordinator, key, name, unit, device_class))
+        sensors.append(HolfuySensor(coordinator, key, name, unit, device_class, station_id))
 
     async_add_entities(sensors)
 
 
 class HolfuySensor(CoordinatorEntity, SensorEntity):
-    def __init__(self, coordinator, key, name, unit, device_class):
+    def __init__(self, coordinator, key, name, unit, device_class, station_id):
         super().__init__(coordinator)
         self._key = key
         self._name = name
         self._unit = unit
         self._device_class = device_class
+        self._station_id = str(station_id)
+        # stable unique id for the entity
+        self._unique_id = f"{self._station_id}_{self._key}"
+
+    @property
+    def unique_id(self):
+        return self._unique_id
 
     @property
     def name(self):
@@ -82,4 +91,19 @@ class HolfuySensor(CoordinatorEntity, SensorEntity):
         return {
             "station_name": data.get("stationName"),
             "last_update": data.get("dateTime"),
+        }
+
+    @property
+    def device_info(self):
+        """Return device information for this entity.
+
+        Entities that return the same identifiers will be grouped under the same device.
+        """
+        data = self.coordinator.data or {}
+        station_name = data.get("stationName") or f"Holfuy {self._station_id}"
+        return {
+            "identifiers": {(DOMAIN, self._station_id)},  # unique device identifier
+            "name": station_name,
+            "manufacturer": "Holfuy",
+            "model": "Weather Station",
         }
